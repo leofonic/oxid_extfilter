9b947d9474c723029f3e08e5ab5b83fab2b71743
Frank Zunderer <info@zunderweb.de>
2015-12-14 17:11:19 +0100

    Corrected Version

--- oxid_4.7-4.9/copy_this/modules/zunderweb/z_extfilter/metadata.php
+++ oxid_4.7-4.9/copy_this/modules/zunderweb/z_extfilter/metadata.php
@@ -6,7 +6,7 @@ $aModule = array(
         'de'=>'Zeigt nach Auswahl eines Attributfilters andere M&ouml;glichkeiten direkt an',
         'en'=>'Shows all attribute values after selecting a value',
     ),
-    'version'     => '1.2',
+    'version'     => '2.1',
     'url'         => 'http://zunderweb.de',
     'email'       => 'info@zunderweb.de',
     'author'      => 'Zunderweb',
